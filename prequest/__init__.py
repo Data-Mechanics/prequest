0fd3de044d5ecc5b5bc9feb0b79bf34a6c720d1d
Frederick Jansen <frederick.jansen@gmail.com>
2018-02-04 22:37:27 -0500

    Override requests Session to allow passthrough for untouched methods (only get() implemented so far)

--- prequest/__init__.py
+++ prequest/__init__.py
@@ -1,16 +1,14 @@
 from .prequest import Prequest
+from requests import Response
 
 __version__ = '0.0.1'
 
 
-def get(url):
+def get(url: str) -> Response:
     """
     Get dataset hosted at a given url
     :param url: Url to fetch data from
     :return: response obtained by data service
     """
-    pq = Prequest()
-    # Does not exist yet.
-    response = pq.get("https://data.boston.gov/export/35f/ad2/35fad26c-1400-46b0-846c-3bb6ca8f74d0.json")
-    response = pq.get(url)
-    return response
+    with Prequest() as pq:
+        return pq.get(url)
--- prequest/prequest.py
+++ prequest/prequest.py
@@ -1,52 +1,52 @@
-import requests
-import json
-
-class Prequest():
-
-	PARENT_API_URL = "https://t4oqhh4fk6.execute-api.us-east-1.amazonaws.com/beta?url={}?returnS3Url={}"
-
-	def get(self,url):
-
-		#Fetch from data source as a default
-		response = requests.get(url)
-		print(self.PARENT_API_URL)
-		cache_response = []
-
-		if(response.status_code == 200):
-			#everything went through
-			#check if data is cached online, Fetch API. 
-			#Pass returnS3Url as False, implies we dont care for any url to be returned. We just want to check existence
-			print("Everything went OK. Checking S3 for pre existing cache")
-
-			cache_response = requests.get(self.PARENT_API_URL.format(url, False))
-			print("cache response status code:" + cache_response.status_code)
-
-			if(cache_response.status_code != 200):
-				#create a cache, call writeData
-				requests.post(self.PARENT_API_URL+url)
-				#else let it be. A cache already exists for this dataset.
-
-			return response
-		else:
-			#Errors 404,400,500, and so on...
-			#Now we need to request S3 url from the cache and proceed to GET it ourselves.
-			#Call the "Fetch API" of AWS
-			#Pass returnS3Url as True, implies we care for a url to be returned. 
-
-			print("Original data not found. Checking S3 for pre existing cache")
-
-			cache_response = requests.get(self.PARENT_API_URL.format(url, True))
-			cache_response =  cache_response.json()
-
-			#If cache response is 200, return cached data. If not, implies cache was never created in the first place. Return original response.
-			return cache_response.status_code == 200 ? requests.get(cache_response.url) : response 
+from requests import Response
+from requests import Session
+from typing import Dict
 
 
+class Prequest(Session):
+    PARENT_API_URL = "https://t4oqhh4fk6.execute-api.us-east-1.amazonaws.com/beta?url={}?returnS3Url={}"
 
+    def request(self, method: str, url: str, params: Dict = None, **kwargs) -> Response:
+        """
+        Sends a request.
+        If the data for a GET is not available, it is returned from cache if possible.
 
-#Sample call
-# pq = Prequest()
-#Does not exist yet.
-# response =  pq.get("https://data.boston.gov/export/35f/ad2/35fad26c-1400-46b0-846c-3bb6ca8f74d0.json");
-# response = pq.get(url);
+        :param method: Method for the new Request object.
+        :param url: URL for the new Request object.
+        :param params: (optional) Dictionary or bytes to be sent in the query string for the Request.
+        :param kwargs: Optional arguments that request takes.
+        :return: Response object
+        """
 
+        # Default behaviour for all methods that are not GET
+        if method != 'GET':
+            return super().request(method, url, params, **kwargs)
+
+        # Fetch from data source as a default
+        resp = super().request('GET', url, params, **kwargs)
+
+        if resp.status_code == 200:
+            print('Everything went OK. Checking S3 for pre existing cache')
+
+            # Let Lambda cache file if necessary
+            # We do not care about the response
+            cache_response = super().request('GET', self.PARENT_API_URL.format(url, False))
+            print('cache response status code: {}'.format(cache_response.status_code))
+
+            return resp
+        else:
+            # The original file is no longer available online, check the cache
+
+            print('Original data not found. Checking S3 for pre existing cache')
+
+            # Get the S3 URL from the cache, this time we do care about the response
+            cache_response = super().request('GET', self.PARENT_API_URL.format(url, True))
+            cache_response = cache_response.json()
+
+            # If cache response is 200, return cached data.
+            if cache_response.status_code == 200:
+                return super().request('GET', cache_response.url)
+            else:
+                # If not, implies cache was never created in the first place.
+                # Return original response and let user handle it
+                return resp
